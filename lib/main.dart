d0cbcc539d75c2c58aa748070138328bcfa836ca
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-14 12:02:36 +0530

    create a state provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_text_scaling_example/screens/home_screen.dart';
-void main(){
+import 'package:riverpod/riverpod.dart';
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
+
+/// Created a StateProvider to manage the text scaling changes
+final scaleProvider = StateProvider((ref) => 1);
